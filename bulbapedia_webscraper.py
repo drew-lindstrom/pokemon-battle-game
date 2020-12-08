e79896d28da70159013bd92a15aa3b763ff02572
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2020-12-08 17:59:38 -0600

    Finished webscraping of desired info. Testing iterating through the list of Pokemon now.

--- bulbapedia_webscraper.py
+++ bulbapedia_webscraper.py
@@ -2,34 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-URL = 'https://bulbapedia.bulbagarden.net/wiki/Bulbasaur_(Pok%C3%A9mon)'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='mw-content-text')
-elems = results.find_all('table', class_='roundy')
 weight_re = re.compile(r'\d+.\d+')
+pokemon_dict = {}
 
-def get_name():
-    pass
+def get_name(soup):
+    name = soup.find(id='mw-content-text').find(class_='roundy').find('b').text
+    return name
 
-def get_type():
+def get_type(soup):
     # get_abilities() and get_weight() are set to sort through all six possible entiries, this function right now only
     # looks for the first type pairing and will need to be updated for alternate forms
     types_table = soup.find('a', title='Type').parent.find_next('table', style='margin:auto; background:none;')
     types = types_table.find_all('td', {'style': lambda x: x != ('display: none; width:50%;' or 'display: none;')})
     return [typ.find('span').text for typ in types]
 
-def get_abilities():
+def get_abilities(soup):
     abilities_table = soup.find('a', title='Ability').parent.find_next('table')
     abilities = abilities_table.find_all('td', {'style': lambda x: x != 'display: none'})
     return [ability.find('span').text for ability in abilities]
 
-def get_weight():
+def get_weight(soup):
     weights_table = soup.find('a', title='Weight').parent.find_next('table')
     weights = weights_table.find_all('tr', {'style': lambda x: x != 'display:none;'})
     return [weight_re.findall(weight.text) for weight in weights]
 
-def get_base_stats():
+def get_base_stats(soup):
+    # There might be issues when a Pokemon has multiple forms with different stats.
     hp = soup.find('tr', style='background: #FF5959; text-align:center').find('div', style='float:right').string
     attack = soup.find('tr', style='background: #F5AC78; text-align:center').find('div', style='float:right').string
     defense = soup.find('tr', style='background: #FAE078; text-align:center').find('div', style='float:right').string
@@ -37,9 +35,25 @@ def get_base_stats():
     sp_def = soup.find('tr', style='background: #A7DB8D; text-align:center').find('div', style='float:right').string
     speed = soup.find('tr', style='background: #FA92B2; text-align:center').find('div', style='float:right').string
 
-    print(hp, attack, defense, sp_attack, sp_def, speed)
+    return (hp, attack, defense, sp_attack, sp_def, speed)
 
-print(get_type())
-print(get_abilities())
-print(get_weight())
-get_base_stats()
+def get_next_pokemon(soup):
+    next_pokemon = soup.find(id='mw-content-text').find(style='text-align: left').a['href']
+    return next_pokemon
+
+def get_data(pokemon_dict, current_URL):
+    page = requests.get(current_URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    pokemon_dict[get_name(soup)] = get_type(soup), get_abilities(soup), get_weight(soup), get_base_stats(soup)
+    next_URL = 'https://bulbpaedia.bulbagarden.net' + get_next_pokemon(soup)
+    return next_URL
+
+current_URL = 'https://bulbapedia.bulbagarden.net/wiki/Bulbasaur_(Pok%C3%A9mon)'
+for x in range(10):
+    try:
+        print(f'Pokemon number: {x}')
+        temp, current_URL = get_data(pokemon_dict, current_URL)
+    except:
+        continue
+print(current_URL)
+print(pokemon_dict)
