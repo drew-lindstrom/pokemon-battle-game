4a4f944ece91e4cf6d211a9ffa33c4854fd39b31
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2020-12-18 22:31:32 -0600

    Updated base stats method

--- bulbapedia_webscraper.py
+++ bulbapedia_webscraper.py
@@ -9,9 +9,18 @@ def get_names(soup):
     """Retreives the name of the pokemon and additional forms (regional variants, mega, gigantamax, etc.) from the current page."""
     # TO DO: Charizard returns two Gigantamax Charizard. Need to get rid of duplicates. Mega Charizard has hex decimal characters.
     # TO DO: Remove Gigantamax names from list. Gigantamax pokemon don't have unique types or abilities.
+
+    names_list = []
+
     html_block = soup.find(id='mw-content-text').find('table', class_='roundy', style='background:#FFF;')
     names_html = html_block.find_all(class_=('image'))
-    return [name['title'] for name in names_html]
+    for name in names_html:
+        # Gigantamax pokemon don't have altered stats, abilites, or types so they are excluded.
+        if name['title'].startswith('Gigantamax'):
+            pass
+        else:
+            names_list.append(name['title'])
+    return names_list
 
 def get_types(soup):
     type_list = []
@@ -26,7 +35,7 @@ def get_types(soup):
 def get_abilities(soup):
     # TO DO: Check to make sure abilities match up with different forms. Slowbro comes to mind.
     html_block = soup.find('a', title='Ability').parent.find_next('table')
-    abilities_html = html_block.find_all('td', {'style': lambda x: x != 'display: none'}).td
+    abilities_html = html_block.find_all('td', {'style': lambda x: x != 'display: none'})
     return [ability.find('span').text for ability in abilities_html]
 
 def get_weights(soup):
@@ -48,61 +57,55 @@ def get_weights(soup):
 
 def get_base_stats(soup):
 
-    hp_html = soup.find_all('tr', style='background: #FF5959; text-align:center')
-    attack_html = soup.find_all('tr', style='background: #F5AC78; text-align:center')
-    defense_html = soup.find_all('tr', style='background: #FAE078; text-align:center')
-    sp_attack_html = soup.find_all('tr', style='background: #9DB7F5; text-align:center')
-    sp_def_html = soup.find_all('tr', style='background: #A7DB8D; text-align:center')
-    speed_html = soup.find_all('tr', style='background: #FA92B2; text-align:center')
-
     stats_list = []
-    print(stats_list)
 
-    # print(hp_html)
-    # for n in range(len(hp_html)):
-    hp = hp_html.find('div', style='float:right').string
-    attack = attack_html.find('div', style='float:right').string      
-    defense = defense_html.find('div', style='float:right').string
-    sp_attack = sp_attack_html.find('div', style='float:right').string
-    sp_def = sp_def_html.find('div', style='float:right').string
-    speed = speed_html.find('div', style='float:right').string
-    
-    stats_list[n].append((hp, attack, defense, sp_attack, sp_def, speed))
+    html_blocks = soup.find_all('a', href='/wiki/Statistic', title='Statistic')
 
-    # hp_html = soup.find_all('tr', style='background: #FF5959; text-align:center')
-    # for hp in hp_html:
-    #     stats_list[0].append(hp.find('div', style='float:right').string)
-    #     counter += 1
-    # attack_html = soup.find_all('tr', style='background: #F5AC78; text-align:center')
-    # for attack in attack_html:
-    #     stats_list[1].append(attack.find('div', style='float:right').string)
-    # defense_html = soup.find_all('tr', style='background: #FAE078; text-align:center')
-    # for defense in defense_html:
-    #     stats_list[2].append(defense.find('div', style='float:right').string)
-    # sp_attack_html = soup.find_all('tr', style='background: #9DB7F5; text-align:center')
-    # for sp_attack in sp_attack_html:
-    #     stats_list[3].append(sp_attack.find('div', style='float:right').string)
-    # sp_def_html = soup.find_all('tr', style='background: #A7DB8D; text-align:center')
-    # for sp_def in sp_def_html:
-    #     stats_list[4].append(sp_def.find('div', style='float:right').string)
-    # speed_html = soup.find_all('tr', style='background: #FA92B2; text-align:center')
-    # for speed in speed_html:
-    #     stats_list[5].append(speed.find('div', style='float:right').string)
+    for html_block in html_blocks: 
+        html_block = html_block.parent.parent.parent
+        hp = html_block.find('tr', style='background: #FF5959; text-align:center').find('div', style='float:right').string
+        attack = html_block.find('tr', style='background: #F5AC78; text-align:center').find('div', style='float:right').string
+        defense = html_block.find('tr', style='background: #FAE078; text-align:center').find('div', style='float:right').string
+        sp_attack = html_block.find('tr', style='background: #9DB7F5; text-align:center').find('div', style='float:right').string
+        sp_def = html_block.find('tr', style='background: #A7DB8D; text-align:center').find('div', style='float:right').string
+        speed = html_block.find('tr', style='background: #FA92B2; text-align:center').find('div', style='float:right').string
+        stats_list.append((hp, attack, defense, sp_attack, sp_def, speed))
 
-    # for n in stats_list[0]:
-    #     print(n)
-        # stats.append(int(stats_list[0][n]))
-        # , stats_list[1][n], stats_list[2][n], stats_list[3][n], stats_list[4][n], stats_list[5][n])
     return stats_list
 
 def get_next_pokemon(soup):
     next_pokemon = soup.find(id='mw-content-text').find(style='text-align: left').a['href']
     return next_pokemon
 
-current_URL = 'https://bulbapedia.bulbagarden.net/wiki/Charizard_(Pokémon)'
+current_URL = 'https://bulbapedia.bulbagarden.net/wiki/Slowbro_(Pokémon)'
 page = requests.get(current_URL)
 soup = BeautifulSoup(page.content, 'html.parser')
-print(get_base_stats(soup))
+
+counter = 0
+type_counter = 0
+names_list = get_names(soup)
+
+for name in names_list:
+
+    types_list = get_types(soup)
+    abilities_list = get_abilities(soup)
+    weights_list = get_weights(soup)
+    base_stats_list = get_base_stats(soup)
+
+    print(len(abilities_list)-len(names_list)+1)
+
+    try:
+        if counter == 0:
+            pokemon_dict[name] = types_list[type_counter:type_counter+2], abilities_list[0:len(abilities_list)-len(names_list)+1], weights_list[counter], base_stats_list[counter]
+        else:
+            pokemon_dict[name] = types_list[type_counter:type_counter+2], abilities_list[counter], weights_list[counter], base_stats_list[counter]
+    except Exception:
+        continue
+    finally:
+        counter += 1
+        type_counter += 2
+
+print(pokemon_dict)
 # pokemon_dict[get_name(soup)] = get_type(soup), get_abilities(soup), get_weight(soup), get_base_stats(soup)
 # print(f'{get_name(soup)}: {pokemon_dict[get_name(soup)]}')
 
