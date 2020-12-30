8b624e12f63340e123c036f00a03927b1a5941bd
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2020-12-29 21:57:39 -0600

    Updated init to initialize stats and natures.

--- pokemon.py
+++ pokemon.py
@@ -1,4 +1,4 @@
-from game_data import pokemon_dict, moves_dict
+from game_data import natures_dict, pokemon_dict, moves_dict
 import math
 
 
@@ -16,8 +16,23 @@ class Pokemon():
         self.base_stats = pokemon_dict[name][1]
         self.stats = {'current_hp': 0, 'max_hp': 0, 'attack': 0, 'defense': 0, 'sp_attack': 0,
                       'sp_defense': 0, 'speed': 0}
-        self.stat_mods = {'attack': 0, 'defense': 0, 'sp_attack': 0,
-                          'sp_defense': 0, 'speed': 0, 'accuracy': 0, 'evasion': 0}
+        self.stat_mods = [1, 1, 1, 1, 1, 1, 1]
+
+        # HP stat calculation is wrong for some reason.
+        self.stats['max_hp'] = int(((2*int(self.base_stats[0])+self.IVs[0]+int(
+            math.sqrt(self.EVs[0])/4))*self.level)/100)+self.level+10
+        self.stats['current_hp'] = self.stats['max_hp']
+
+        stats_list = [0, 0, 0, 0, 0]
+        for n in range(5):
+            stats_list[n] = int((int(((2*int(self.base_stats[n+1])+self.IVs[n+1]+int(
+                math.sqrt(self.EVs[n+1])/4))*self.level)/100)+5)*natures_dict[self.nature][n])
+
+        self.stats["attack"] = stats_list[0]
+        self.stats["defense"] = stats_list[1]
+        self.stats["sp_attack"] = stats_list[2]
+        self.stats["sp_defense"] = stats_list[3]
+        self.stats["speed"] = stats_list[4]
 
     def show_stats(self):
         """Prints the stats of the Pokemon with modifiers applied."""  # TO DO: Add modifiers
@@ -33,12 +48,7 @@ class Pokemon():
         print(f'Speed: {self.stats["speed"]}')
         print()
 
-    def init_stats(self):
-        self.stats['max_hp'] = (int(((int(self.base_stats[0])+self.IVs[0])*2+int(
-            math.sqrt(self.level)/4))*self.level)/100)+self.level+10
-
 
 abomasnow = Pokemon('Abomasnow', 100, 'Male', None,
-                    None, None, (31, 31, 31, 31, 31, 31), (255, 0, 0, 0, 0, 0), None)
-abomasnow.init_stats()
+                    None, None, (31, 31, 31, 31, 31, 31), (255, 0, 0, 0, 0, 0), 'Adamant')
 Pokemon.show_stats(abomasnow)
