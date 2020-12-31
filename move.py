b66daac6d3dc492437d6e37a32b8bc9cf5effee4
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2020-12-30 21:56:58 -0600

    Initial print.

---
+++ move.py
@@ -0,0 +1,26 @@
+from game_data import moves_dict
+
+
+class Move():
+    def __init__(self, name):
+        self.name = name
+        self.type = moves_dict[name][0]
+        self.category = moves_dict[name][1]
+        self.power = moves_dict[name][2]
+        self.accuracy = moves_dict[name][3]
+        self.pp = moves_dict[name][4]
+        self.max_pp = self.pp
+
+    def show_stats(self):
+
+        print('Move: ', self.name)
+        print(f'Type: {self.type}')
+        print(f'Category: {self.category}')
+        print(f'Power: {self.power}')
+        print(f'Accuracy: {self.accuracy}')
+        print(f'PP: {self.pp}/{self.max_pp}')
+        print()
+
+
+mach_punch = Move('Mach Punch')
+Move.show_stats(mach_punch)
