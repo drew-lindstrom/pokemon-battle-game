50cf06484953bd709a6d40c9c52b21ff4e520f43
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-01-08 21:37:23 -0600

    Minor updates to structure

--- abilities.py
+++ abilities.py
@@ -12,3 +12,18 @@ def aerilate(attacker, n):
         #Power boost by 20% (Note: not overall damage boost)
 
 def aftermath(attacker, n, defender):
+    if attacker.ability == 'Damp':
+        pass
+    if time == "post_damage" and defender.hp == 0 and attacker.move[n].contact == True:
+        #Damage attacker 1/4 of its max HP.
+
+def airlock(attacker, n, defender):
+    # Negates the effects of weather but doesn't eliminate the weather?
+
+def analytic(attacker, n, defender):
+    if attacker.turn == "Last":
+        #Power boost by 30%
+
+def anger_point(attacker, n, defender):
+    if time == "post_damage":
+        #Raise attack to +6 if hit with a critical hit.
