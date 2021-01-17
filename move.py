46aed4269dbdcc1084cfcc5bb5a30ecf77251b0a
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-01-17 13:08:16 -0600

    Updated class to use properties.

--- move.py
+++ move.py
@@ -1,6 +1,5 @@
 from game_data import moves_dict
-
-# TODO: Adjust max PP
+from copy import deepcopy
 
 
 class Move:
@@ -8,10 +7,41 @@ class Move:
         self.name = name
         self.type = moves_dict[name][0]
         self.category = moves_dict[name][1]
-        self.power = moves_dict[name][2]
-        self.accuracy = moves_dict[name][3]
-        self.pp = moves_dict[name][4]
-        self.max_pp = self.pp
+        self.pp = None
+
+    @property
+    def power(self):
+        power = moves_dict[self.name][2]
+        if power == None:
+            return 0
+        return int(power)
+
+    @property
+    def accuracy(self):
+        accuracy = moves_dict[self.name][3]
+        if accuracy == None:
+            return 0
+        return int(accuracy)
+
+    @property
+    def max_pp(self):
+        max_pp = int(moves_dict[self.name][4])
+        if max_pp <= 1:
+            return max_pp
+        return int(max_pp * 1.6)
+
+    @property
+    def pp(self):
+        return self._pp
+
+    @pp.setter
+    def pp(self, n=None):
+        if n == None or n > self.max_pp:
+            self._pp = self.max_pp
+        elif n <= 0:
+            self._pp = 0
+        else:
+            self._pp = n
 
     def show_stats(self):
 
@@ -22,6 +52,26 @@ class Move:
         print(f"Accuracy: {self.accuracy}")
         print(f"PP: {self.pp}/{self.max_pp}")
         print()
+
+    def check_pp(self):
+        """Returns True if a move has enough PP to be used (above 0 PP). Returns False otherwise.
+        move.pp (int) -> Boolean
+        Ex: move.pp == 0, return False.
+        Ex: move.pp == 3, return True."""
+        if self.pp <= 0:
+            print(f"{self.name} is out of PP!")
+            return False
+        return True
+
+
+earthquake = Move("Earthquake")
+print(earthquake.pp)
+earthquake.pp = -5
+print(earthquake.pp)
+earthquake.pp = 99
+print(earthquake.pp)
+earthquake.pp = 7
+print(earthquake.pp)
 
 
 def frost_breath():
