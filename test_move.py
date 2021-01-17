b34c9f739acd68be4cdccfac9a4cf53acf5be51e
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-01-17 15:18:42 -0600

    Updated init tests.

--- test_move.py
+++ test_move.py
@@ -3,6 +3,33 @@ import pytest
 
 
 class TestMove:
+    def test_init_move(self):
+        earthquake = Move("Earthquake")
+        bulk_up = Move("Bulk Up")
+
+        assert earthquake.type == "Ground"
+        assert earthquake.category == "Physical"
+        assert earthquake.power == 100
+        assert earthquake.accuracy == 100
+        assert earthquake.max_pp == 16
+        assert earthquake.pp == 16
+
+        assert bulk_up.type == "Fighting"
+        assert bulk_up.category == "Status"
+        assert bulk_up.power == 0
+        assert bulk_up.accuracy == 0
+        assert bulk_up.max_pp == 32
+        assert bulk_up.pp == 32
+
+    def test_pp(self):
+        earthquake = Move("Earthquake")
+        earthquake.pp = -5
+        assert earthquake.pp == 0
+        earthquake.pp = 99
+        assert earthquake.pp == 16
+        earthquake.pp = 7
+        assert earthquake.pp == 7
+
     def test_check_pp(self):
         earthquake = Move("Earthquake")
         assert earthquake.check_pp() == True
--- test_pokemon.py
+++ test_pokemon.py
@@ -4,7 +4,7 @@ from pokemon import Pokemon
 
 class TestPokemon:
     def test_init_stats(self):
-        testPokemon = Pokemon(
+        test_pokemon = Pokemon(
             "Slowbro",
             100,
             "Male",
@@ -15,10 +15,32 @@ class TestPokemon:
             (252, 0, 252, 0, 4, 0),
             "Relaxed",
         )
-        assert testPokemon.hp == 394
-        assert testPokemon.max_hp == 394
-        assert testPokemon.attack == 186
-        assert testPokemon.defense == 350
-        assert testPokemon.sp_attack == 236
-        assert testPokemon.sp_defense == 197
-        assert testPokemon.speed == 86
+        assert test_pokemon.hp == 394
+        assert test_pokemon.max_hp == 394
+        assert test_pokemon.attack == 186
+        assert test_pokemon.defense == 350
+        assert test_pokemon.sp_attack == 236
+        assert test_pokemon.sp_defense == 197
+        assert test_pokemon.speed == 86
+
+    def test_heal(self):
+        test_pokemon = Pokemon(
+            "Slowbro",
+            100,
+            "Male",
+            ("Scald", "Slack Off", "Future Sight", "Teleport"),
+            None,
+            None,
+            (31, 31, 31, 31, 31, 31),
+            (252, 0, 252, 0, 4, 0),
+            "Relaxed",
+        )
+        test_pokemon.hp = 150
+        test_pokemon.heal(0.5)
+        assert test_pokemon.hp == 347
+        test_pokemon.hp = 393
+        test_pokemon.heal(0.5)
+        assert test_pokemon.hp == 394
+        test_pokemon.hp = 0
+        test_pokemon.heal(0)
+        assert test_pokemon.hp == 0
