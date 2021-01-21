7e409e22fee880539eb3770e7f4cb31c2896f53d
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-01-20 21:40:27 -0600

    Minor bug fix.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from team import Team
 from teams import player1, player2
 
 
-def print_current_pokemon(pokemon1, pokemon2):
+def print_pokemon_on_field(pokemon1, pokemon2):
     print(
         f"{pokemon1.name} - HP: {pokemon1.hp}/{pokemon1.max_hp}, Status: {pokemon1.status}"
     )
@@ -14,23 +14,34 @@ def print_current_pokemon(pokemon1, pokemon2):
     print()
 
 
-def get_choice(pokemon):
-    # TODO: add pp check
+def get_choice(team):
     choice = None
-    while choice not in ("0", "1", "2", "3", "4", "5", "6"):
-        print(f"What will {pokemon.name} do?")
+    current_pokemon = team.current_pokemon
+
+    while choice not in range(10):
+        print(f"What will {current_pokemon.name} do?")
         print()
-        for n in range(len(pokemon.moves)):
+        for n in range(len(current_pokemon.moves)):
             print(
-                f"({n+1}) {pokemon.moves[n].name} - {pokemon.moves[n].pp}/{pokemon.moves[n].max_pp} PP"
+                f"({n+1}) {current_pokemon.moves[n].name} - {current_pokemon.moves[n].pp}/{current_pokemon.moves[n].max_pp} PP"
             )
+        print()
         print("(5) Switch Pokemon")
-        print("(6) Move Details")
+        print("(6) Details")
         print()
 
-        choice = str(int(input()) - 1)
+        choice = int(input()) - 1
         print()
-    return choice
+
+        if choice == 4:
+            choice = get_switch_choice(team)
+        elif choice == 5:
+            current_pokemon.show_stats()
+            choice = None
+        else:
+            if current_pokemon.moves[n].check_pp() == False:
+                choice = None
+    return int(choice)
 
 
 def get_switch_choice(team):
@@ -51,15 +62,15 @@ def get_switch_choice(team):
         choice = input()
         print()
 
-    team.switch(choice)
-    return None
+    choice = int(choice) + 5
+    return choice
 
 
-def turn_order_check(player1, player2):
+def find_turn_order(player1, player2):
     try:
-        if player1.team_list[0].adj_speed > player2.team_list[0].adj_speed:
+        if player1.current_pokemon.speed > player2.current_pokemon.speed:
             return [player1, player2]
-        # If the speed check is a tie, usually its random who goes first, but for the sake of consistency, the opponent will always go first.
+        # If the speed check is a tie, its usually random who goes first, but for the sake of AI consistency, the opponent will always go first.
         else:
             return [player2, player1]
     except:
@@ -71,31 +82,29 @@ def clear_screen():
         print()
 
 
-while True:
-    clear_screen()
-    turn_order = turn_order_check(player1, player2)
-    switch_order = []
-    attack_order = []
-    attack_choice = []
+clear_screen()
 
-    print_current_pokemon(player1.team_list[0], player2.team_list[0])
+while True:
+    turn_order = find_turn_order(player1, player2)
+    switch_list = []
+    attack_list = []
+
+    print_pokemon_on_field(player1.current_pokemon, player2.current_pokemon)
 
     for x in range(len(turn_order)):
-        choice = get_choice(turn_order[x].team_list[0])
+        choice = get_choice(turn_order[x])
         clear_screen()
-        if choice == "5":
-            switch_order.append(turn_order[x])
-            clear_screen()
-        else:
-            attack_order.append(turn_order[x])
-            attack_choice.append(int(choice))
-            clear_screen()
-    for x in range(len(switch_order)):
-        get_switch_choice(switch_order[x])
-        clear_screen()
+        if choice >= 5:
+            switch_list.append([turn_order[x], choice - 5])
 
-    for x in range(len(attack_order)):
-        if attack_order[x] == player1:
-            attack(player1.team_list[0], attack_choice[x], player2.team_list[0])
+        elif choice in range(4):
+            attack_list.append([turn_order[x], choice])
+
+    for x in range(len(switch_list)):
+        switch_list[x][0].switch(switch_list[x][1])
+
+    for x in range(len(attack_list)):
+        if attack_list[x][0] == player1:
+            attack(player1.current_pokemon, attack_list[x][1], player2.current_pokemon)
         else:
-            attack(player2.team_list[0], attack_choice[x], player1.team_list[0])
+            attack(player2.current_pokemon, attack_list[x][1], player1.current_pokemon)
