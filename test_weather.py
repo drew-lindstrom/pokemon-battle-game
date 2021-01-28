3a2fc809257e31f07d9bad515b27217964e0d344
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-01-27 21:32:57 -0600

    Added sandstorm damage funciton.

--- test_weather.py
+++ test_weather.py
@@ -1,5 +1,5 @@
 from pokemon import Pokemon
-from weather import Weather
+from weather import Weather, sandstorm_damage
 import pytest
 
 
@@ -18,3 +18,66 @@ class Test_Weather:
         weather.weather_counter = 0
         weather.clear_weather()
         assert weather.current_weather == "Clear Skies"
+
+    def test_sandstorm_damage(self):
+        weather = Weather("Sandstorm", 5)
+        slowbro = Pokemon(
+            "Slowbro",
+            100,
+            "Male",
+            ("Scald", "Slack Off", "Future Sight", "Teleport"),
+            None,
+            None,
+            (31, 31, 31, 31, 31, 31),
+            (252, 0, 252, 0, 4, 0),
+            "Relaxed",
+        )
+        tyranitar = Pokemon(
+            "Tyranitar",
+            100,
+            "Male",
+            ("Scald", "Slack Off", "Future Sight", "Teleport"),
+            None,
+            None,
+            (31, 31, 31, 31, 31, 31),
+            (252, 0, 252, 0, 4, 0),
+            "Relaxed",
+        )
+        torterra = Pokemon(
+            "Torterra",
+            100,
+            "Male",
+            ("Scald", "Slack Off", "Future Sight", "Teleport"),
+            None,
+            None,
+            (31, 31, 31, 31, 31, 31),
+            (252, 0, 252, 0, 4, 0),
+            "Relaxed",
+        )
+        metagross = Pokemon(
+            "Metagross",
+            100,
+            "Male",
+            ("Scald", "Slack Off", "Future Sight", "Teleport"),
+            None,
+            None,
+            (31, 31, 31, 31, 31, 31),
+            (252, 0, 252, 0, 4, 0),
+            "Relaxed",
+        )
+        pikachu = Pokemon(
+            "Slowbro",
+            100,
+            "Male",
+            ("Scald", "Slack Off", "Future Sight", "Teleport"),
+            None,
+            "Safety Goggles",
+            (31, 31, 31, 31, 31, 31),
+            (252, 0, 252, 0, 4, 0),
+            "Relaxed",
+        )
+        assert sandstorm_damage(weather, slowbro) == True
+        assert sandstorm_damage(weather, tyranitar) == False
+        assert sandstorm_damage(weather, torterra) == False
+        assert sandstorm_damage(weather, metagross) == False
+        assert sandstorm_damage(weather, pikachu) == False
--- weather.py
+++ weather.py
@@ -1,15 +1,50 @@
 from pokemon import Pokemon
 
-
+# Need to add weather method descriptions
 class Weather:
-    def __init__(self):
-        self.current_weather = "Clear Skies"
-        self.weather_counter = None
+    def __init__(self, weather="Clear Skies", counter=None):
+        self.current_weather = weather
+        self.weather_counter = counter
 
     def set_weather(self, weather, counter):
         self.current_weather = weather
         self.weather_counter = counter
+        if self.current_weather == "Sandstorm":
+            print("A sandstorm kicked up!")
 
     def clear_weather(self):
         if self.weather_counter == 0:
+            if self.current_weather == "Sandstorm":
+                print("The sandstorm subsided.")
             self.current_weather = "Clear Skies"
+
+
+def sandstorm_damage(weather, pokemon):
+    if pokemon.typing[0] in ("Rock", "Steel", "Ground") or pokemon.typing[1] in (
+        "Rock",
+        "Steel",
+        "Ground",
+    ):
+        return False
+    elif pokemon.ability in (
+        "Sand Force",
+        "Sand Rush",
+        "Sand Veil",
+        "Magic Guard",
+        "Overcoat",
+    ):
+        return False
+    elif pokemon.item == "Safety Goggles":
+        return False
+    else:
+        pokemon.damage(1 / 16)
+        return True
+
+    # Sandstorm To Do:
+    #  Effect Text
+    #  Ability activation
+    #  Special defense boost for rock types
+    #  Changes Weather Ball to a rock-type move and doubles its power
+    #  Halves power of solar beam and solar blade
+    #  Causes Shore Up to recover 2/3 of max HP instead of 1/2
+    #  Causes Moonlight, Synthesis, and Morning Sun to recover 1/4 of max HP
