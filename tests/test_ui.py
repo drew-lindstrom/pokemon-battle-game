fce41aba0fc000b4ac954eb96367800b70c0fe30
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-15 12:06:22 -0500

    Initial commit.

---
+++ tests/test_ui.py
@@ -0,0 +1,20 @@
+import pytest
+
+# class TestUI:
+#     def test_get_choice(self):
+#             slowbro = Pokemon(
+#             "Slowbro",
+#             100,
+#             "Male",
+#             ("Scald", "Slack Off", "Future Sight", "Teleport"),
+#             None,
+#             None,
+#             (31, 31, 31, 31, 31, 31),
+#             (252, 0, 252, 0, 4, 0),
+#             "Relaxed",
+#         )
+#         team = [slowbro]
+
+#         assert get_choice(team)
+
+#     def test_get_switch(self):
