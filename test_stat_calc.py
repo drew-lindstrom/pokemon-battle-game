473b380d16939581a9ae6e0b0f406d52ce5c29f2
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-18 11:56:54 -0500

    Initial commit.

---
+++ test_stat_calc.py
@@ -0,0 +1,19 @@
+from stat_calc import calc_attack, calc_defense, calc_sp_attack, calc_sp_defense, calc_speed, calc_accuracy, calc_evasion
+import pytest
+
+class TestStatCalc():
+    def test_calc_attack(self):
+    
+    def test_calc_defense(self):
+    
+    def test_calc_sp_attack(self):
+    
+    def test_calc_sp_defense(self):
+    
+    def test_calc_speed(self):
+    
+    def test_calc_accuracy(self):
+    
+    def test_calc_evasion(self):
+        
+
