1713664b8531090177a9d89ca72ded810c347581
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-18 11:24:55 -0500

    Implemented calc_attack and general structure for the other functions.

--- stat_calc.py
+++ stat_calc.py
@@ -1,23 +1,53 @@
-def calc_stat():
-    #TODO: If critical hit, ignore stat some stat modifiers.
+from pokemon import Pokemon
 
-def calc_attack():
-    pass
+# TODO: If critical hit, ignores negative attack modifiers, positive defense modifiers, and defensive boosts from auroa veil, light screen, and reflect
+# TODO: Add in light screen, reflect, and aurora veil
+def calc_attack(cur_pokemon, stat, crit):
+    """Calculates the attack stat of the given pokemon by calculating its modified attack stat and mulitplying it with any additional modifiers.
+    If the given attack roled a critical hit, a negative attack stat_mod is ignored and calc_modified_stat is not called.
+    Additional modifiers are still applied."""
+    additional_modifier = 1
 
-def calc_defense():
-    pass
+    if cur_pokemon.item == "Choice Band":
+        additional_modifier *= 1.5
+    if cur_pokemon.status == "Burn":
+        additional_modifier *= 0.5
 
-def calc_sp_attack():
-    pass
+    if crit == True and cur_pokemon.stat_mod["attack"] < 0:
+        return pokemon.stat["attack"] * additional_modifier
+    return pokemon.calc_modified_stat["attack"] * additional_modifier
 
-def calc_sp_defense():
-    pass
 
-def calc_speed():
-    pass
+def calc_defense(attacker, stat, crit):
+    additional_modifier = 1
+    return pokemon.calc_modified_stat["defense"] * additional_modifier
 
-def calc_accuracy():
-    pass
 
-def calc_evasion():
+def calc_sp_attack(attacker, stat, crit):
+    additional_modifier = 1
+    if pokemon.item == "Choice Spec":
+        additional_modifier *= 1.5
+
+    return pokemon.calc_modified_stat["sp_attack"] * additional_modifier
+
+
+def calc_sp_defense(attacker, stat, crit):
+    additional_modifier = 1
+
+    return pokemon.calc_modified_stat["sp_defense"] * additional_modifier
+
+
+def calc_speed(attacker, stat, crit):
+    additional_modifier = 1
+    if pokemon.item == "Choice Scarf":
+        additional_modifier *= 1.5
+
+    return pokemon.calc_modified_stat["speed"] * additional_modifier
+
+
+def calc_accuracy(attacker, stat, crit):
+    additional_modifier
+
+
+def calc_evasion(attacker, stat, crit):
     pass
