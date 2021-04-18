ee91e8d445e49462479a0b3f915b670e02d42803
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-18 11:36:37 -0500

    Implemented calc_sp_attack, calc_sp_defense, and calc_speed.

--- stat_calc.py
+++ stat_calc.py
@@ -2,7 +2,7 @@ from pokemon import Pokemon
 
 # TODO: If critical hit, ignores negative attack modifiers, positive defense modifiers, and defensive boosts from auroa veil, light screen, and reflect
 # TODO: Add in light screen, reflect, and aurora veil
-def calc_attack(team, stat, crit):
+def calc_attack(team, crit):
     """Calculates the attack stat of the given pokemon by calculating its modified attack stat and mulitplying it with any additional modifiers.
     If the given attack roled a critical hit, a negative attack stat_mod is ignored and calc_modified_stat is not called.
     Additional modifiers are still applied."""
@@ -19,7 +19,7 @@ def calc_attack(team, stat, crit):
     return pokemon.calc_modified_stat["attack"] * additional_modifier
 
 
-def calc_defense(team, stat, crit):
+def calc_defense(team, crit):
     """Calculates the defense stat of the given pokemon by calculating its modified defense stat and mulitplying it with any additional modifiers.
     If the given attack roled a critical hit, a positive defense stat_mod is ignored and calc_modified_stat is not called.
     Defense boost from reflect is also ignored if present.
@@ -35,32 +35,49 @@ def calc_defense(team, stat, crit):
     return pokemon.calc_modified_stat["defense"] * additional_modifier
 
 
-def calc_sp_attack(attacker, stat, crit):
-
+def calc_sp_attack(team, crit):
+    """Calculates the special attack stat of the given pokemon by calculating its modified sp_attack stat and mulitplying it with any additional modifiers.
+    If the given attack roled a critical hit, a negative sp_attack stat_mod is ignored and calc_modified_stat is not called.
+    Additional modifiers are still applied."""
     additional_modifier = 1
     if pokemon.item == "Choice Spec":
         additional_modifier *= 1.5
-
+    if crit == True and cur_pokemon.stat_mod["sp_attack"] < 0:
+        return pokemon.stat["sp_attack"] * additional_modifier
     return pokemon.calc_modified_stat["sp_attack"] * additional_modifier
 
 
-def calc_sp_defense(attacker, stat, crit):
+def calc_sp_defense(team, crit):
+    """Calculates the special defense stat of the given pokemon by calculating its modified sp_defense stat and mulitplying it with any additional modifiers.
+    If the given attack roled a critical hit, a positive sp_defense stat_mod is ignored and calc_modified_stat is not called.
+    Defense boost from light screen is also ignored if present.
+    Additional modifiers are still applied."""
+    cur_pokemon = team.cur_pokemon
     additional_modifier = 1
 
-    return pokemon.calc_modified_stat["sp_defense"] * additional_modifier
+    if player.light_screen == True and crit == False:
+        additional_modifier *= 1.5
+
+    if crit == True and cur_pokemon.stat_mod["defense"] > 0:
+        return pokemon.stat["defense"] * additional_modifier
+    return pokemon.calc_modified_stat["defense"] * additional_modifier
 
 
-def calc_speed(attacker, stat, crit):
+def calc_speed(team, crit):
+    """Calculates the speed stat of the given pokemon by calculating its modified speed stat and mulitplying it with any additional modifiers."""
+    cur_pokemon = team.cur_pokemon
     additional_modifier = 1
     if pokemon.item == "Choice Scarf":
         additional_modifier *= 1.5
+    if pokemon.status == "Paralyzed":
+        additional_modifier *= 0.5
 
     return pokemon.calc_modified_stat["speed"] * additional_modifier
 
 
-def calc_accuracy(attacker, stat, crit):
+def calc_accuracy(team, crit):
     additional_modifier
 
 
-def calc_evasion(attacker, stat, crit):
+def calc_evasion(team, crit):
     pass
