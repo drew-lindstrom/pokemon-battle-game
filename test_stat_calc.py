ce929f6b3d9f0611c5b7964ca83f46cc2f6a3b46
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-19 17:46:44 -0500

    Implemented test_calc_sp_attack and test_calc_speed.

--- test_stat_calc.py
+++ test_stat_calc.py
@@ -88,7 +88,39 @@ class TestStatCalc:
         assert calc_defense(team, True) == 70
 
     def test_calc_sp_attack(self):
-        pass
+        p1 = Pokemon(
+            "Slowbro",
+            100,
+            "Male",
+            ("Scald", "Slack Off", "Future Sight", "Teleport"),
+            None,
+            None,
+            (31, 31, 31, 31, 31, 31),
+            (0, 0, 0, 0, 0, 0),
+            "Relaxed",
+        )
+        team = Player([p1])
+        assert calc_sp_attack(team, False) == 236
+        assert calc_sp_attack(team, True) == 236
+        p1.item = "Choice Spec"
+        assert calc_sp_attack(team, False) == 354
+        assert calc_sp_attack(team, True) == 354
+
+        p1.item = None
+        p1.stat_mod["sp_attack"] = 6
+        assert calc_sp_attack(team, False) == 944
+        assert calc_sp_attack(team, True) == 944
+        p1.item = "Choice Spec"
+        assert calc_sp_attack(team, False) == 1416
+        assert calc_sp_attack(team, True) == 1416
+
+        p1.item = None
+        p1.stat_mod["sp_attack"] = -6
+        assert calc_sp_attack(team, False) == 59
+        assert calc_sp_attack(team, True) == 236
+        p1.item = "Choice Spec"
+        assert calc_sp_attack(team, False) == 88
+        assert calc_sp_attack(team, True) == 354
 
     def test_calc_sp_defense(self):
         p1 = Pokemon(
@@ -113,7 +145,50 @@ class TestStatCalc:
         assert calc_sp_defense(team, True) == 49
 
     def test_calc_speed(self):
-        pass
+        p1 = Pokemon(
+            "Slowbro",
+            100,
+            "Male",
+            ("Scald", "Slack Off", "Future Sight", "Teleport"),
+            None,
+            None,
+            (31, 31, 31, 31, 31, 31),
+            (0, 0, 0, 0, 0, 0),
+            "Relaxed",
+        )
+        team = Player([p1])
+        assert calc_speed(team, False) == 86
+        p1.item = "Choice Scarf"
+        assert calc_speed(team, False) == 129
+        p1.item = None
+        p1.status = "Paralyzed"
+        assert calc_speed(team, False) == 43
+        p1.item = "Choice Scarf"
+        assert calc_speed(team, False) == 64
+
+        p1.item = None
+        p1.status = None
+        p1.stat_mod["speed"] = 6
+        assert calc_speed(team, False) == 344
+        p1.item = "Choice Scarf"
+        assert calc_speed(team, False) == 516
+        p1.item = None
+        p1.status = "Paralyzed"
+        assert calc_speed(team, False) == 172
+        p1.item = "Choice Scarf"
+        assert calc_speed(team, False) == 258
+
+        p1.item = None
+        p1.status = None
+        p1.stat_mod["speed"] = -6
+        assert calc_speed(team, False) == 21
+        p1.item = "Choice Scarf"
+        assert calc_speed(team, False) == 31
+        p1.item = None
+        p1.status = "Paralyzed"
+        assert calc_speed(team, False) == 10
+        p1.item = "Choice Scarf"
+        assert calc_speed(team, False) == 15
 
     def test_calc_accuracy(self):
         pass
