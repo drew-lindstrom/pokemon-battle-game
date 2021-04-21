34237d02a642ec7713e74b3298859c4e961fddeb
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-21 15:16:03 -0500

    Added functions for checking flinched, choice item, encored, taunted, and disabled.

---
+++ attack_verification.py
@@ -0,0 +1,65 @@
+from pokemon import Pokemon
+
+
+def check_flinched(attacker, move_name):
+    if "Flinched" in attacker.volatile_statuses:
+        print(f"{attacker.name} flinched!")
+        return True
+
+
+def check_choice_item():
+    if (
+        "Choice Locked" in attacker.violatile_statuses
+        and previous_move != None
+        and previous_move != move_name
+    ):
+        print(f"{attacker.name} can only use {attacker.previous_move}")
+        return False
+
+
+def check_encored():
+    if (
+        "Encored" in attacker.violatile_statuses
+        and previous_move != None
+        and previous_move != move_name
+    ):
+        print(f"{attacker.name} can only use {attacker.previous_move}")
+        return False
+
+
+def check_taunted():
+    if (
+        "Taunted" in attacker.violatile_statuses
+        and attacker.moves[n].category == "Status"
+    ):
+        print(f"{attacker.name} must use at attacking move while taunted.")
+        return False
+
+
+def check_disabled():
+    if (
+        "Disabled" in attacker.violatile_statuses
+        and attacker.violatile_statuses["Disabled"][1] == move_name
+    ):
+        print(f"{attacker.name} is not able to use {move_name} while it is disabled.")
+        return False
+
+
+def check_semi_invulnerable_turn():
+    pass
+
+
+def check_recharging():
+    pass
+
+
+def check_charging_turn():
+    pass
+
+
+def check_tormented():
+    pass
+
+
+def pp_check():
+    pass
