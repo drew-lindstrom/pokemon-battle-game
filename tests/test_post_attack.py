d30d9a8cf82c0067de421e65169cb9a5d677a69b
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-26 17:28:52 -0500

    Implemented tests for apply_burn and apply_bad_poison.

--- tests/test_post_attack.py
+++ tests/test_post_attack.py
@@ -1,4 +1,4 @@
-from post_attack import apply_leftovers
+from post_attack import apply_leftovers, apply_burn, apply_bad_poison
 from pokemon import Pokemon
 import pytest
 
@@ -22,3 +22,51 @@ class TestPostAttack:
         slowbro.hp = 380
         apply_leftovers(slowbro)
         assert slowbro.stat["hp"] == 394
+
+    def test_apply_burn(self):
+        p = Pokemon(
+            "Slowbro",
+            100,
+            "Male",
+            ("Scald", "Slack Off", "Future Sight", "Teleport"),
+            None,
+            "Leftovers",
+            (31, 31, 31, 31, 31, 31),
+            (252, 0, 252, 0, 4, 0),
+            "Relaxed",
+        )
+
+        apply_burn(p)
+        assert p.stat["hp"] == 394
+        p.status = ["Burned"]
+        apply_burn(p)
+        assert p.stat["hp"] == 369
+        p.stat["hp"] = 5
+        apply_burn(p)
+        assert p.stat["hp"] == 0
+
+    def test_apply_bad_poison(self):
+        p = Pokemon(
+            "Slowbro",
+            100,
+            "Male",
+            ("Scald", "Slack Off", "Future Sight", "Teleport"),
+            None,
+            "Leftovers",
+            (31, 31, 31, 31, 31, 31),
+            (252, 0, 252, 0, 4, 0),
+            "Relaxed",
+        )
+
+        apply_bad_poison(p)
+        assert p.stat["hp"] == 394
+        p.status = ["Badly Poisoned", 0]
+        apply_bad_poison(p)
+        assert p.stat["hp"] == 369
+        p.status = ["Badly Poisoned", 1]
+        apply_bad_poison(p)
+        assert p.stat["hp"] == 319
+        p.stat["hp"] = 394
+        p.status = ["Badly Poisoned", 20]
+        apply_bad_poison(p)
+        assert p.stat["hp"] == 24
