12675717bf2b02f61a4ec972f5359dc14b383587
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-26 17:27:56 -0500

    Fixed minor bug.

--- post_attack.py
+++ post_attack.py
@@ -19,7 +19,7 @@ def apply_bad_poison(pokemon):
     """Damages a baldy poison pokemon with increasingly higher damage at the end of every turn. Initially deals 1/16 of max HP
     but adds an addition 1/16 damage (up until 15 * floor(max hp/16)) every turn the pokemon is in. If the pokemon switches out,
     the damage resets to the original 1/16 of max HP."""
-    if pokemon.status[0] == "Baldly Poisoned":
+    if pokemon.status[0] == "Badly Poisoned":
         print(f"{pokemon.name} was hurt by the poison!")
         if pokemon.status[1] >= 14:
             pokemon.damage(0.0625 * 15)
