eb11f2f3c89ac99f31e8ca166fa67fc0f8b1f01b
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-04-26 17:18:36 -0500

    Implemented apply_burn and apply_bad_poison.

--- post_attack.py
+++ post_attack.py
@@ -6,3 +6,22 @@ def apply_leftovers(pokemon):
     if pokemon.item == "Leftovers":
         pokemon.heal(0.0625)
         print(f"{pokemon.name} healed some of it's HP with it's leftovers.")
+
+
+def apply_burn(pokemon):
+    """Damages a burned pokemon by 1/16 of its max HP. Fire type pokemon cannot be burned."""
+    if pokemon.status[0] == "Burned":
+        print(f"{pokemon.name} was damaged by its burn!")
+        pokemon.damage(0.0625)
+
+
+def apply_bad_poison(pokemon):
+    """Damages a baldy poison pokemon with increasingly higher damage at the end of every turn. Initially deals 1/16 of max HP
+    but adds an addition 1/16 damage (up until 15 * floor(max hp/16)) every turn the pokemon is in. If the pokemon switches out,
+    the damage resets to the original 1/16 of max HP."""
+    if pokemon.status[0] == "Baldly Poisoned":
+        print(f"{pokemon.name} was hurt by the poison!")
+        if pokemon.status[1] >= 14:
+            pokemon.damage(0.0625 * 15)
+        else:
+            pokemon.damage(0.0625 * (pokemon.status[1] + 1))
