1f1036b247e4fd328a3fd93f86a8a820b66a4013
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-05-04 16:46:38 -0500

    Updated roll_crit, check_stab, and check_type_effectiveness.

--- damage_calc.py
+++ damage_calc.py
@@ -6,68 +6,36 @@ from game_data import type_key, type_chart
 from pokemon import Pokemon
 
 
-def next_turn(pokemon_1, action_1, pokemon_2, action_2):
-    pass
-
-
-# TODO: Add optional i parameter.
-def roll_crit():
+def roll_crit(i=None):
     """Rolls to determine if a move lands a critical hit. Critical hits boost damage by 1.5 ignore the attacker's negative stat stages,
     the defender's positive stat stages, and Light Screen/Reflect/Auorar Veil. Burn is not ignored."""
-    # TO DO: add special moves/itesms/abilities.
-    crit = randint(1, 24)
-    if crit == 1:
+    if i is None or i < 0 or i > 24:
+        i = randint(1, 24)
+    if i == 1:
         print("A critical hit!")
         return 1.5
     else:
         return 1
 
 
-# TODO: Add optional i parameter.
-def roll_accuracy(attacker, n, defender):
-    """Rolls to determine if a move lands or misses."""
-    num = 3
-    den = 3
-    modifier = attacker.stat_mods[5] - defender.stat_mods[6]
-
-    if modifier > 6:
-        modifier = 6
-    elif modifier < -6:
-        modifier = -6
-    if modifier > 0:
-        num = num + modifier
-    elif modifier < 0:
-        den = den + (modifier * -1)
-
-    accuracy = int(attacker.moves[n].accuracy) * int(num / den)
-    check = randint(1, 100)
-    if check <= accuracy:
-        return True
-    else:
-        return False
-
-
-def check_stab(attacker, n):
+def check_stab(user, attack):
     """Checks to see if the attacking move is the same type as the attacker. If so, attack power is boosted by 50%."""
-    if (
-        attacker.typing[0] == attacker.moves[n].type
-        or attacker.typing[1] == attacker.moves[n].type
-    ):
+    if attacker.typing[0] == attack.type or attacker.typing[1] == attack.type:
         return 1.5
     else:
         return 1
 
 
-def check_type_effectiveness(attacker, n, defender):
+def check_type_effectiveness(user, target, attack):
     """Return the damage multiplier for how super effective the move is. type_chart is a matrix showing how each type matches up between each
     other. X-axis is the defending type, y-axis is the attacking type. Top left corner is (0, 0). Each type corresponds to a number on the
     x and y axis."""
 
-    atk_id = type_key.get(attacker.moves[n].type)
-    def1_id = type_key.get(defender.typing[0])
+    atk_id = type_key.get(attack.type)
+    def1_id = type_key.get(target.typing[0])
     mult_1 = type_chart[atk_id][def1_id]
     try:
-        def2_id = type_key.get(defender.typing[1])
+        def2_id = type_key.get(target.typing[1])
         mult_2 = type_chart[atk_id][def2_id]
     except:
         mult_2 = 1
@@ -135,8 +103,7 @@ Slack Off
 
 def attack(attacker, n, defender):
     """Determines if a move hits and how much damage is dealt."""
-    #  TO DO: Critical hit ignore thes attacker's negative stat stages, the defender's positive stat stages, and Light Screen/Reflect/Auorar Veil.
-    # TO DO: Added in stat modifiers and create a damage calc function.
+    #  TODO: Critical hit ignore thes attacker's negative stat stages, the defender's positive stat stages, and Light Screen/Reflect/Auorar Veil.
     # print(f'{attacker.name} used {attacker.moves[n]["name"]}!')
     print(f"{attacker.name} used {attacker.moves[n].name}!")
     if accuracy_check(attacker, n, defender):
@@ -178,19 +145,3 @@ def attack(attacker, n, defender):
             print("It's not very effective...")
         elif typ == 0:
             print("It had no effect...")
-
-    else:
-        if attacker.name.endswith("s"):
-            print(f"{attacker.name}' attack missed!")
-        else:
-            print(f"{attacker.name}'s attack missed!")
-
-    attacker.moves[n].pp = int(attacker.moves[n].pp) - 1
-
-    if defender.hp <= 0:
-        defender.hp = 0
-        defender.status = "Fainted"
-        print(f"{defender.name} fainted!")
-    # How to force a switch?
-
-    print()
