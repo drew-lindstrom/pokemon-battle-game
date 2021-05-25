5ecd8fad7433af018152c9a0014c6cabae984257
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-05-25 16:04:32 -0500

    Add function descriptions.

--- switch_effects.py
+++ switch_effects.py
@@ -4,26 +4,31 @@ from weather import Weather
 
 
 def activate_grassy_surge(user, terrain):
+    """Sets terrain to grassy terrain, if possible, when pokemon with grassy surge ability switches in."""
     if user.ability == "Grassy Surge":
         terrain.set_terrain("Grassy Terrain")
 
 
 def activate_intimidate(user, target):
+    """Lowers target's attack by one when a pokemon with intimidate ability switches in."""
     if user.ability == "Intimidate":
         print(f"{target.name} was initimated!")
         target.update_stat_modifier("attack", -1)
 
 
 def activate_regenerator(user):
+    """Heals a pokemon by 1/3 of it's max hp if it's ability is regenerator and switches out."""
     if user.ability == "Regenerator":
         user.heal(1 / 3)
 
 
 def activate_psychic_surge(user, terrain):
+    """Sets terrain to psychic terrain, if possible, when pokemon with psychic surge ability switches in."""
     if user.ability == "Psychic Surge":
         terrain.set_terrain("Psychic Terrain")
 
 
 def activate_sand_stream(user, weather):
+    """Sets weather to sandstorm, if possible, when pokemon with sand stream ability switches in."""
     if user.ability == "Sand Stream":
         weater.set_weather("Sandstorm")
