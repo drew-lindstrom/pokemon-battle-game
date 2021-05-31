37a656d1e001c661a437e498ffdda5a97207e4cb
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-05-31 15:13:04 -0500

    Implemented tests for get_switch and get_choice.

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,26 +1,72 @@
-import pytest
-import ui
-from io import StringIO
+from ui import *
+from main import Frame
 from pokemon import Pokemon
 from player import Player
+from weather import Weather
+from terrain import Terrain
+import pytest
 
 
-# class TestUI:
-#     def test_get_choice(self, monkeypatch):
-#         slowbro = Pokemon(
-#             "Slowbro",
-#             100,
-#             "Male",
-#             ("Scald", "Slack Off", "Future Sight", "Teleport"),
-#             None,
-#             None,
-#             (31, 31, 31, 31, 31, 31),
-#             (252, 0, 252, 0, 4, 0),
-#             "Relaxed",
-#         )
-#         team = Player([slowbro])
-#         input_ = StringIO("0")
-#         monkeypatch.setattr("builtins.input", input_)
-#         assert ui.get_choice(team) == (team, "Scald", 0)
+class TestUI:
+    @pytest.fixture
+    def test_frame(self):
+        slowbro = Pokemon(
+            "Slowbro",
+            100,
+            "Male",
+            ("Scald", "Slack Off", "Future Sight", "Teleport"),
+            None,
+            None,
+            (31, 31, 31, 31, 31, 31),
+            (252, 0, 252, 0, 4, 0),
+            "Relaxed",
+        )
+        tyranitar = Pokemon(
+            "Tyranitar",
+            100,
+            "Male",
+            ("Crunch", "Stealth Rock", "Toxic", "Earthquake"),
+            None,
+            None,
+            (31, 31, 31, 31, 31, 31),
+            (252, 0, 0, 0, 216, 40),
+            "Careful",
+        )
+        tapu_lele = Pokemon(
+            "Tapu Lele",
+            100,
+            None,
+            ("Psychic", "Moonblast", "Focus Blast", "Psyshock"),
+            "Psychic Surge",
+            "Choice Specs",
+            (31, 0, 31, 31, 31, 31),
+            (0, 0, 0, 252, 4, 252),
+            "Timid",
+        )
+        cinderace = Pokemon(
+            "Cinderace",
+            100,
+            "Male",
+            ("Pyro Ball", "U-turn", "Gunk Shot", "High Jump Kick"),
+            "Libero",
+            "Heavy Duty Boots",
+            (31, 31, 31, 31, 31, 31),
+            (0, 252, 0, 0, 4, 252),
+            "Jolly",
+        )
+        p1 = Player([slowbro, tyranitar])
+        p2 = Player([tapu_lele, cinderace])
+        w = Weather()
+        t = Terrain()
+        test_frame = Frame(p1, p2, None, None, w, t)
+        return test_frame
 
-#     # def test_get_switch(self):
+    def test_get_switch(self, test_frame):
+        get_switch(test_frame, ["3", "a", "1"])
+        assert test_frame.switch_choice == "1"
+
+    def test_get_choice(self, test_frame):
+        get_choice(test_frame, ["8", "-4", "b", "2"])
+        assert test_frame.attack == test_frame.user.moves[1]
+        get_choice(test_frame, ["5", "1"])
+        assert test_frame.switch_choice == "1"
