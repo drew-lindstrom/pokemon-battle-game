a6151fad270c1134396b739536c2c8d2f663e778
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-06-11 14:15:01 -0500

    Updated tests with parametrization where applicable.

--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -18,15 +18,21 @@ class TestPokemon:
         )
         return test_pokemon
 
-    def test_init_stats(self, test_pokemon):
+    @pytest.mark.parametrize(
+        "stat_name,expected_int",
+        [
+            ("hp", 394),
+            ("max_hp", 394),
+            ("attack", 186),
+            ("defense", 350),
+            ("sp_attack", 236),
+            ("sp_defense", 197),
+            ("speed", 86),
+        ],
+    )
+    def test_init_stats(self, test_pokemon, stat_name, expected_int):
         p = test_pokemon
-        assert p.stat["hp"] == 394
-        assert p.stat["max_hp"] == 394
-        assert p.stat["attack"] == 186
-        assert p.stat["defense"] == 350
-        assert p.stat["sp_attack"] == 236
-        assert p.stat["sp_defense"] == 197
-        assert p.stat["speed"] == 86
+        assert p.stat[stat_name] == expected_int
 
     def test_set_move(self, test_pokemon):
         test_pokemon.set_move(0, "Close Combat")
@@ -35,17 +41,19 @@ class TestPokemon:
         assert test_pokemon.moves[0].power == 120
         assert test_pokemon.moves[0].accuracy == 100
         assert test_pokemon.moves[0].category == "Physical"
+        assert test_pokemon.moves[0].max_pp == 8
+        assert test_pokemon.moves[0].pp == 8
 
-    def test_update_stat_modifier(self, test_pokemon):
+    @pytest.mark.parametrize(
+        "stat_name,input_int,expected_int",
+        [("attack", 4, 4), ("attack", 8, 6), ("attack", -7, -6)],
+    )
+    def test_update_stat_modifier(
+        self, test_pokemon, stat_name, input_int, expected_int
+    ):
         p = test_pokemon
-        p.update_stat_modifier("attack", 4)
-        assert p.stat_mod["attack"] == 4
-        p.update_stat_modifier("attack", 4)
-        assert p.stat_mod["attack"] == 6
-        p.update_stat_modifier("attack", -7)
-        assert p.stat_mod["attack"] == -1
-        p.update_stat_modifier("attack", -7)
-        assert p.stat_mod["attack"] == -6
+        p.update_stat_modifier(stat_name, input_int)
+        assert p.stat_mod[stat_name] == expected_int
 
     def test_reset_stat_modifier(self, test_pokemon):
         p = test_pokemon
@@ -68,67 +76,89 @@ class TestPokemon:
         assert p.calc_modified_stat("accuracy") == int(7 / 3 * 100)
         assert p.calc_modified_stat("evasion") == int(3 / 7 * 100)
 
-    def test_apply_heal(self, test_pokemon):
+    @pytest.mark.parametrize(
+        "input_hp,heal_percentage,expected_hp",
+        [(150, 0.5, 347), (393, 0.5, 394), (0, 0, 0)],
+    )
+    def test_apply_heal(self, test_pokemon, input_hp, heal_percentage, expected_hp):
         p = test_pokemon
-        p.stat["hp"] = 150
-        p.apply_heal(0.5)
-        assert p.stat["hp"] == 347
-        p.stat["hp"] = 393
-        p.apply_heal(0.5)
-        assert p.stat["hp"] == 394
-        p.stat["hp"] = 0
-        p.apply_heal(0)
-        assert p.stat["hp"] == 0
+        p.stat["hp"] = input_hp
+        p.apply_heal(heal_percentage)
+        assert p.stat["hp"] == expected_hp
 
-    def test_apply_damage(self, test_pokemon):
+    @pytest.mark.parametrize(
+        "input_hp,damage_amount,damage_percentage,expected_hp,expected_status",
+        [
+            (200, 50, None, 150, None),
+            (35, 50, None, 0, "Fainted"),
+            (394, None, 0.5, 197, None),
+            (35, None, 0.5, 0, "Fainted"),
+        ],
+    )
+    def test_apply_damage(
+        self,
+        test_pokemon,
+        input_hp,
+        damage_amount,
+        damage_percentage,
+        expected_hp,
+        expected_status,
+    ):
         p = test_pokemon
-        p.stat["hp"] = 200
-        p.apply_damage(50, None)
-        assert p.stat["hp"] == 150
-        p.stat["hp"] = 35
-        p.apply_damage(50, None)
-        assert p.stat["hp"] == 0
-        assert p.status[0] == "Fainted"
+        p.stat["hp"] = input_hp
+        p.apply_damage(damage_amount, damage_percentage)
+        assert p.stat["hp"] == expected_hp
+        assert p.status[0] == expected_status
 
-        p.stat["hp"] = p.stat["max_hp"]
-        p.apply_damage(None, 0.5)
-        assert p.stat["hp"] == 197
-        p.stat["hp"] = 35
-        p.apply_damage(None, 0.5)
-        assert p.stat["hp"] == 0
-
-    def test_check_fainted(self, test_pokemon):
+    @pytest.mark.parametrize(
+        "input_hp,expected_hp,expected_bool,expected_status",
+        [(200, 200, False, None), (0, 0, True, "Fainted"), (-4, 0, True, "Fainted")],
+    )
+    def test_check_fainted(
+        self, test_pokemon, input_hp, expected_hp, expected_bool, expected_status
+    ):
         p = test_pokemon
-        assert p.check_fainted() == False
-        p.stat["hp"] = 0
-        assert p.check_fainted() == True
-        assert p.status[0] == "Fainted"
-        p.stat["hp"] = -4
-        assert p.check_fainted() == True
-        assert p.stat["hp"] == 0
+        p.stat["hp"] = input_hp
+        assert p.check_fainted() == expected_bool
+        assert p.stat["hp"] == expected_hp
+        assert p.status[0] == expected_status
 
-    def test_struggle_check(self, test_pokemon):
+    @pytest.mark.parametrize(
+        "move_1_pp,move_2_pp,move_3_pp,move_4_pp,expected_bool",
+        [
+            (5, 5, 5, 5, False),
+            (0, 5, 5, 5, False),
+            (0, 0, 5, 5, False),
+            (0, 0, 0, 5, False),
+            (0, 0, 0, 0, True),
+        ],
+    )
+    def test_struggle_check(
+        self, test_pokemon, move_1_pp, move_2_pp, move_3_pp, move_4_pp, expected_bool
+    ):
         p = test_pokemon
-        assert p.struggle_check() == False
-        p.moves[0].pp = 0
-        assert p.struggle_check() == False
-        p.moves[1].pp = 0
-        assert p.struggle_check() == False
-        p.moves[2].pp = 0
-        assert p.struggle_check() == False
-        p.moves[3].pp = 0
-        assert p.struggle_check() == True
+        p.moves[0].pp = move_1_pp
+        p.moves[1].pp = move_2_pp
+        p.moves[2].pp = move_3_pp
+        p.moves[3].pp = move_4_pp
+        assert p.struggle_check() == expected_bool
 
-    def test_set_status(self, test_pokemon):
+    @pytest.mark.parametrize(
+        "starting_status,input_status,expected_result",
+        [
+            ([None, 0], None, [None, 0]),
+            ([None, 0], "Paralyzed", ["Paralyzed", 0]),
+            (["Paralyzed", 0], "Asleep", ["Paralyzed", 0]),
+            ([None, 0], "Badly Poisoned", ["Badly Poisoned", 14]),
+        ],
+    )
+    def test_set_status(
+        self, test_pokemon, starting_status, input_status, expected_result
+    ):
         p = test_pokemon
-        assert p.status == [None, 0]
-        p.set_status("Paralyzed")
-        assert p.status == ["Paralyzed", 0]
-        p.set_status("Asleep")
-        assert p.status == ["Paralyzed", 0]
-        p.status = [None, 0]
-        p.set_status("Badly Poisoned")
-        assert p.status == ["Badly Poisoned", 14]
+        p.status = starting_status
+        p.set_status(input_status)
+        assert p.status == expected_result
 
     def test_cure_status(self, test_pokemon):
         p = test_pokemon
@@ -167,30 +197,33 @@ class TestPokemon:
         assert p.status == ["Badly Poisoned", 14]
         assert len(p.v_status) == 0
 
-    def test_check_grounded(self, test_pokemon):
+    @pytest.mark.parametrize(
+        "input_ability,input_item,input_v_status_name,input_v_status_number,expected_bool",
+        [
+            (None, None, None, None, True),
+            ("Levitate", None, None, None, False),
+            ("Levitate", "Iron Ball", None, None, True),
+            (None, "Air Balloon", None, None, False),
+            (None, "Air Balloon", "Ingrained", [1], True),
+            (None, None, "Magnet Rise", [4], False),
+            (None, None, "Telekinesis", [3], False),
+        ],
+    )
+    def test_check_grounded(
+        self,
+        test_pokemon,
+        input_ability,
+        input_item,
+        input_v_status_name,
+        input_v_status_number,
+        expected_bool,
+    ):
         p = test_pokemon
-        assert p.grounded == True
-        p.ability = "Levitate"
+        p.ability = input_ability
+        p.item = input_item
+        p.v_status[input_v_status_name] = input_v_status_number
         p.check_grounded()
-        assert p.grounded == False
-        p.item = "Iron Ball"
-        p.check_grounded()
-        assert p.grounded == True
-        p.item = "Air Balloon"
-        p.ability = None
-        p.check_grounded()
-        assert p.grounded == False
-        p.v_status["Ingrained"] = [3]
-        p.check_grounded()
-        assert p.grounded == True
-        del p.v_status["Ingrained"]
-        p.v_status["Magnet Rise"] = [4]
-        p.check_grounded()
-        assert p.grounded == False
-        del p.v_status["Magnet Rise"]
-        p.v_status["Telekinesis"] = [3]
-        p.check_grounded()
-        assert p.grounded == False
+        assert p.grounded == expected_bool
 
     def test_set_prev_move(self, test_pokemon):
         p = test_pokemon
