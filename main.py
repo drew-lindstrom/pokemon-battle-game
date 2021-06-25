122ede935ad11da0fd04034bc7cf8495a702969a
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-06-25 11:55:04 -0500

    Refactored main function into many smaller more managable functions.

--- main.py
+++ main.py
@@ -24,6 +24,34 @@ def main():
     w = Weather()
     t = Terrain()
 
+    activateTurnOneSwitchAbilities(p1, p2, w, t)
+
+    while gameOverBool is False:
+
+        frame1, frame2 = applyPreInputPreparations(p1, p2, w, t)
+        getPlayerInputs(frame1, frame2)
+        ui.clearScreen()
+        # Determines which player goes first for the turn (based on speed, priority moves, etc.)
+        frameOrder = getFrameOrder(frame1, frame2)
+
+        for curFrame in frameOrder:
+            if curFrame.switchChoice:
+                applySwitch(curFrame, frame1, frame2)
+
+            elif curFrame.user.status[0] != "Fainted":
+                print(f"{curFrame.user.name} used {curFrame.attack.name}!")
+                print()
+
+                if checkIfCanAttackAndAttackLands(curFrame):
+                    applyAttack(curFrame)
+
+        gameOverBool = applyEndOfTurnEffects(frameOrder, w, t, gameOverBool)
+
+        if gameOverBool:
+            break
+
+
+def activateTurnOneSwitchAbilities(p1, p2, w, t):
     #'Switches' leading pokemon with their respective selves in order to activate any abilities that activate on switch in.
     openingFrame1 = Frame(p1, p2, None, None, w, t)
     openingFrame1.switchChoice = 0
@@ -32,72 +60,91 @@ def main():
     switch(openingFrame1)
     switch(openingFrame2)
 
-    while gameOverBool is False:
-        frame1 = Frame(p1, p2, None, None, w, t)
-        frame2 = Frame(p2, p1, None, None, w, t)
 
-        ui.printPokemonOnField(frame1, frame2)
+def applyPreInputPreparations(p1, p2, w, t):
+    frame1 = Frame(p1, p2, None, None, w, t)
+    frame2 = Frame(p2, p1, None, None, w, t)
 
-        frame1.user.checkChoiceItem()
-        frame2.user.checkChoiceItem()
+    ui.printPokemonOnField(frame1, frame2)
 
-        # Gets input on what each player wants to do before the given turn.
-        ui.getChoice(frame1)
-        ai.chooseHighestDamagingAttack(frame2)
+    frame1.user.checkChoiceItem()
+    frame2.user.checkChoiceItem()
 
-        ui.clearScreen()
+    return frame1, frame2
 
-        # Determines which player goes first for the turn (based on speed, priority moves, etc.)
-        frameOrder = getFrameOrder(frame1, frame2)
 
-        for curFrame in frameOrder:
-            if curFrame.switchChoice:
-                switch(curFrame)
-                frame1.updateCurPokemon()
-                frame2.updateCurPokemon()
-            elif curFrame.user.status[0] != "Fainted":
-                print(f"{curFrame.user.name} used {curFrame.attack.name}!")
-                print()
+def getPlayerInputs(frame1, frame2):
+    ui.getChoice(frame1)
+    ai.chooseHighestDamagingAttack(frame2)
 
-                curFrame.canAttack = checkCanAttack(curFrame)
-                checkAttackLands(curFrame)
-                if curFrame.canAttack and curFrame.attackLands:
-                    if (
-                        curFrame.attack.category == "Physical"
-                        or curFrame.attack.category == "Special"
-                    ):
-                        curFrame.attackDamage = calcDamage(curFrame)
-                        curFrame.target.applyDamage(curFrame.attackDamage, None)
 
-                    else:
-                        applyNonDamagingMove(curFrame)
-                    applyPostAttackEffects(curFrame)
+def applySwitch(curFrame, frame1, frame2):
+    switch(curFrame)
+    frame1.updateCurPokemon()
+    frame2.updateCurPokemon()
 
-        applyEndOfTurnEffects(frameOrder)
 
-        w.decrementWeather()
-        t.decrementTerrain()
+def checkIfCanAttackAndAttackLands(frame):
+    checkCanAttack(frame)
+    checkAttackLands(frame)
+    return frame.canAttack and frame.attackLands
 
-        for curFrame in frameOrder:
-            player = curFrame.attackingTeam
-            # Game over check
-            if player.checkGameOver():
-                if player == p1:
-                    print("Player 2 Wins!")
-                elif player == p2:
-                    print("Player 1 Wins!")
-                gameOverBool = True
-                break
-            # Prompts player to switch any fainted pokemon at end of turn.
-            if player.curPokemon.status[0] == "Fainted":
-                if player == p1:
-                    ui.getSwitch(curFrame)
-                    switch(curFrame)
-                if player == p2:
-                    ai.chooseNextPokemon(curFrame)
-                    switch(curFrame)
+
+def applyAttack(frame):
+    category = frame.attack.category
+
+    if category == "Physical" or category == "Special":
+        frame.attackDamage = calcDamage(frame)
+        frame.target.applyDamage(frame.attackDamage, None)
+    else:
+        applyNonDamagingMove(frame)
+
+    applyPostAttackEffects(frame)
+
+
+def applyEndOfTurnEffects(frameOrder, w, t, gameOverBool):
+    applyEndOfTurnAttackEffects(frameOrder)
+
+    w.decrementWeather()
+    t.decrementTerrain()
+
+    if checkForGameOver(frameOrder):
+        gameOverBool = True
+        return gameOverBool
+
+    checkForFaintedPokemon(frameOrder)
+    return gameOverBool
+
+
+def checkForGameOver(frameOrder):
+    for curFrame in frameOrder:
+        player = curFrame.attackingTeam
+        # Game over check
+        if player.checkGameOver():
+            if player == p1:
+                print("Player 2 Wins!")
+            elif player == p2:
+                print("Player 1 Wins!")
+            return True
+
+    return False
+
+
+def checkForFaintedPokemon(frameOrder):
+    for curFrame in frameOrder:
+        player = curFrame.attackingTeam
+        if player.curPokemon.status[0] == "Fainted":
+            getAppropriateSwitchChoice(curFrame)
+            switch(curFrame)
+
+
+def getAppropriateSwitchChoice(frame):
+    player = frame.attackingTeam
+    if player == p1:
+        ui.getSwitch(frame)
+    if player == p2:
+        ai.chooseNextPokemon(frame)
 
 
 if __name__ == "__main__":
-
     main()
