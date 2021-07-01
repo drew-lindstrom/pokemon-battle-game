c5dd8f5bb863ddf7d3fd0b2b25ae5bc501a58a6c
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-07-01 10:48:22 -0500

    Updated printOptions function to print both moves and pokemon to switch with.

--- v2 Web App/ui.py
+++ v2 Web App/ui.py
@@ -9,7 +9,7 @@ def printPokemonOnField(frame1, frame2):
     for frame in frameOrder:
         gameText.output += f"{frame.user.name} - HP: {frame.user.stat['hp']}/{frame.user.stat['maxHp']}, Status: {frame.user.status[0]}\n"
         gameText.output += f"Attack: {stat_calc.calcAttack(frame)}/{frame.user.stat['attack']}   Defense: {stat_calc.calcDefense(frame, 'user')}/{frame.user.stat['defense']}   Special Attack: {stat_calc.calcSpAttack(frame)}/{frame.user.stat['spAttack']}\n"
-        gameText.output += f"Special Defense: {stat_calc.calcSpDefense(frame, 'user')}/{frame.user.stat['spDefense']}   Speed: {stat_calc.calcSpeed(frame)}/{frame.user.stat['speed']}\n"
+        gameText.output += f"Special Defense: {stat_calc.calcSpDefense(frame, 'user')}/{frame.user.stat['spDefense']}   Speed: {stat_calc.calcSpeed(frame)}/{frame.user.stat['speed']}\n\n"
 
 
 def getChoice(frame, inputList=[], printTextBool=False):
@@ -39,52 +39,6 @@ def getNextChoice(frame, inputList=[]):
         return 0
 
 
-# def callAppropriateFunctionBasedOnChoice(
-#     frame, choice, inputList=[], printTextBool=False
-# ):
-#     if choice >= 1 and choice <= 4:
-#         if checkIfValidChoice(frame, choice, printTextBool):
-#             return True
-
-#     elif choice == 5:
-#         callGetSwitchFunction(frame, inputList)
-#         return True
-
-#     elif choice == 6:
-#         printUserStats(frame)
-#     return False
-
-
-# def checkIfValidChoice(frame, choice, printTextBool=False):
-#     if checkIfChoiceHasEnoughPP(
-#         frame, choice, printTextBool
-#     ) and checkIfUserHasMoveLock(frame, choice, printTextBool):
-#         frame.attack = frame.user.moves[choice - 1]
-#         return True
-#     return False
-
-
-# def checkIfChoiceHasEnoughPP(frame, choice, printTextBool=False):
-#     if frame.user.moves[choice - 1].pp <= 0:
-#         if printTextBool:
-#             print(f"{frame.user.moves[choice - 1].name} is out of PP.")
-#             print()
-#         return False
-#     return True
-
-
-# def checkIfUserHasMoveLock(frame, choice, printTextBool=False):
-#     if "Move Lock" in frame.user.vStatus and (
-#         frame.user.prevMove != None
-#         and frame.user.prevMove != frame.user.moves[choice - 1].name
-#     ):
-#         if printTextBool:
-#             print(f"{frame.user.name} must use {frame.user.prevMove}.")
-#             print()
-#         return False
-#     return True
-
-
 def callGetSwitchFunction(frame, inputList=[]):
     if len(inputList) > 0:
         getSwitch(frame, inputList)
@@ -95,14 +49,12 @@ def printUserStats(frame):
     frame.user.showStats()
 
 
-def printOptions(team):
-    gameText.output += f"What will {team.curPokemon.name} do?\n"
-    for n in range(len(team.curPokemon.moves)):
-        gameText.output += f"({n+1}) {team.curPokemon.moves[n].name} - {team.curPokemon.moves[n].pp}/{team.curPokemon.moves[n].maxPp} PP\n"
-    print()
-    print("(5) Switch Pokemon")
-    print("(6) Details")
-    print()
+def printOptions(frame):
+    gameText.output += f"What will {frame.user.name} do?\n"
+    for n in range(len(frame.user.moves)):
+        gameText.output += f"({n}) {frame.user.moves[n].name} - {frame.user.moves[n].pp}/{frame.user.moves[n].maxPp} PP\n"
+    for n in range(1, len(frame.attackingTeam)):
+        gameText.output += f"({n+3}) {frame.attackingTeam[n].name} - {frame.attackingTeam[n].stat['hp']}/{frame.attackingTeam[n].stat['maxHp']} HP, Status: {frame.attackingTeam[n].status[0]}\n"
 
 
 def getSwitch(frame, inputList=[]):
@@ -122,10 +74,7 @@ def getSwitch(frame, inputList=[]):
 
 def printSwitchChoices(frame):
     print(f"Switch {frame.user.name} with...?")
-    for n in range(1, len(frame.attackingTeam)):
-        print(
-            f"({n}) {frame.attackingTeam[n].name} - {frame.attackingTeam[n].stat['hp']}/{frame.attackingTeam[n].stat['maxHp']} HP, Status: {frame.attackingTeam[n].status[0]}"
-        )
+
     print()
 
 
