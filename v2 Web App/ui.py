d23616433b26a3b7c203a369529798dd85da8505
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-07-01 10:49:42 -0500

    Removed unnessary functions that were used for command line interface version.

--- v2 Web App/ui.py
+++ v2 Web App/ui.py
@@ -12,82 +12,12 @@ def printPokemonOnField(frame1, frame2):
         gameText.output += f"Special Defense: {stat_calc.calcSpDefense(frame, 'user')}/{frame.user.stat['spDefense']}   Speed: {stat_calc.calcSpeed(frame)}/{frame.user.stat['speed']}\n\n"
 
 
-def getChoice(frame, inputList=[], printTextBool=False):
-    choice = None
-
-    while choice is None and choice not in range(1, 7):
-        printOptions(frame.attackingTeam)
-        choice = getNextChoice(frame, inputList)
-
-        if callAppropriateFunctionBasedOnChoice(
-            frame, choice, inputList, printTextBool
-        ):
-            return frame
-        choice = None
-
-
-def getNextChoice(frame, inputList=[]):
-    if len(inputList) == 0:
-        choice = input()
-    else:
-        choice = inputList.pop(0)
-    print()
-
-    try:
-        return int(choice)
-    except Exception:
-        return 0
-
-
-def callGetSwitchFunction(frame, inputList=[]):
-    if len(inputList) > 0:
-        getSwitch(frame, inputList)
-    getSwitch(frame)
-
-
-def printUserStats(frame):
-    frame.user.showStats()
-
-
 def printOptions(frame):
     gameText.output += f"What will {frame.user.name} do?\n"
     for n in range(len(frame.user.moves)):
         gameText.output += f"({n}) {frame.user.moves[n].name} - {frame.user.moves[n].pp}/{frame.user.moves[n].maxPp} PP\n"
     for n in range(1, len(frame.attackingTeam)):
         gameText.output += f"({n+3}) {frame.attackingTeam[n].name} - {frame.attackingTeam[n].stat['hp']}/{frame.attackingTeam[n].stat['maxHp']} HP, Status: {frame.attackingTeam[n].status[0]}\n"
-
-
-def getSwitch(frame, inputList=[]):
-    teamList = []
-    switchChoice = ""
-
-    for n in range(1, len(frame.attackingTeam)):
-        teamList.append(n)
-
-    while switchChoice not in teamList:
-        printSwitchChoices(frame)
-        switchChoice = getNextSwitchChoice(frame, inputList)
-        switchChoice = checkIfSwitchChoiceHasFainted(frame, switchChoice)
-
-    frame.switchChoice = switchChoice
-
-
-def printSwitchChoices(frame):
-    print(f"Switch {frame.user.name} with...?")
-
-    print()
-
-
-def getNextSwitchChoice(frame, inputList=[]):
-    if len(inputList) == 0:
-        switchChoice = input()
-    else:
-        switchChoice = inputList.pop(0)
-
-    try:
-        return int(switchChoice)
-    except Exception:
-        return 0
 
 
 def checkIfSwitchChoiceHasFainted(frame, switchChoice):
@@ -101,8 +31,3 @@ def checkIfSwitchChoiceHasFainted(frame, switchChoice):
     except Exception:
         switchChoice = 0
     return switchChoice
-
-
-def clearScreen():
-    for n in range(17):
-        print()
