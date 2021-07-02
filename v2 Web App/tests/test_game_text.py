464375579fb802675590c9c3e161bee55c8c7acd
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-07-02 14:05:19 -0500

    Initial commit.

---
+++ v2 Web App/tests/test_game_text.py
@@ -0,0 +1,5 @@
+import gameText
+
+gameText.output = []
+
+# There aren't any tests for this module. This is here just to initialize the output variable that other tests require to function without error.
